564540c9aab3332b4528c66bc9a21f3131db53f8
ratamovic <ratamovic@gmail.com>
2014-06-11 00:45:41 +0200

    Gradle configuration

--- leakeeper-core/src/androidTest/java/com/codexperiments/leakeeper/test/TaskManagerTest.java
+++ leakeeper-core/src/androidTest/java/com/codexperiments/leakeeper/test/TaskManagerTest.java
@@ -4,11 +4,6 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
 import android.support.v4.app.FragmentActivity;
 
-/**
- * TODO Failure cases.
- * 
- * TODO keepResultOnHold cases.
- */
 public class TaskManagerTest extends ActivityInstrumentationTestCase2<TaskActivity> {
     public TaskManagerTest() {
         super(TaskActivity.class);
